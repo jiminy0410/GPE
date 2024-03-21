e1276b02efa27809e18259563583745faa6bca7c
Jiminy <jiminy0410@gmail.com>
2024-03-21 14:35:31 +0100

    dinsdag

--- Assets/Interactive Snow/Scripts/FollowPlayer.cs
+++ Assets/Interactive Snow/Scripts/FollowPlayer.cs
@@ -6,19 +6,26 @@ public class FollowPlayer : MonoBehaviour
 {
     [SerializeField]
     Transform target;
+    private Vector3 offset;
     [SerializeField]
     float offsetx;
     [SerializeField]
     float offsetz;
+    [SerializeField]
+    float offsety;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.position - target.position;
+        offsetx = offset.x;
+        offsety = offset.y;
+        offsetz = offset.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.position.x + offsetx, transform.position.y, target.position.z + offsetz);
+        transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, target.position.z + offset.z);
+        //transform.position = new Vector3(target.position.x, target.position.y, target.position.z);
     }
 }
